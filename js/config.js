b7b1cb7116ef2ceef2c7354aa89acfe1398ee72b
ch0939305 <ch0939305@users.noreply.github.com>
2026-07-25 21:00:25 +0800

    修正 Supabase 連線設定

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // 請將以下兩個值替換為你的 Supabase 專案資訊
-const SUPABASE_URL = 'https://kwctohbkxltkarglgxix.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_eTL4-cAGjcOpTXmIu8B0Ug_rup6bo1N';
+const SUPABASE_URL = 'https://sjh28ztp8.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_trNkNRT_tP8ZyFQhZ0V_Nw_koEEJUYo';
